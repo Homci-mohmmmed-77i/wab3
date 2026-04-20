dcfce74fd2e335133501cda6a89981eb1cf967a2
محمد لخضر حمصي <hmsy0594@gmail.com>
2026-04-20 08:31:49 +0100

    Create User.php

---
+++ models/User.php
@@ -0,0 +1,10 @@
+<?php
+class User {
+    public static function findByEmail($email) {
+        global $pdo;
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        return $stmt->fetch();
+    }
+}
+?>
